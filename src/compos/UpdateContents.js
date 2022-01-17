ab5c117f5a27ec103b404df8d5c3296a3799e0fd
UihyunLee <88185154+leeuihyun@users.noreply.github.com>
2022-01-17 21:34:20 +0900

    React today (#3)
    * React CRUD Practice
    
    * React Create 기능 및 Update(selected_id 확인까지)
    
    * Update tech

--- src/compos/UpdateContents.js
+++ src/compos/UpdateContents.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class UpdateContents extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            title : this.props.data.title,
+            desc : this.props.data.desc
+        }
+    }
+    inputFormHandler(e){
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
     render(){
         console.log(this.props.data);
         return(
-            
+
             <footer>
                 <h1>Update</h1>
                 <form action = "/update_process" method = "post" onSubmit = {function(e){
@@ -12,8 +25,23 @@ class UpdateContents extends React.Component{
                     const et = e.target;
                     this.props.onAddContents(et.title.value, et.desc.value);  
                 }.bind(this)}>
-                    <p><input type="text" placeholder = "title" name = "title"/></p>
-                    <p><input type="textarea" placeholder = "description" name = "desc"/></p>
+
+                    <p><input 
+                        type="text" 
+                        placeholder = "title" 
+                        name = "title"
+                        value = {this.state.title}
+                        onChange = {
+                            this.inputFormHandler.bind(this)
+                        }
+                    /></p>
+                    <p><textarea placeholder = "description" 
+                        name = "desc" value = {this.state.desc}
+                        onChange = {
+                            this.inputFormHandler.bind(this)
+                        }
+                    /></p>
+
                     <p><input type="submit" /></p>
                 </form>
             </footer>
